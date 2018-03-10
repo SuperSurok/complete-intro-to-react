b93905b1ca545a2962169da0bdc3f6a34fba3499
Sergey Lebedev <oddyone@mail.ru>
2018-03-10 22:28:38 +0300

    Testing the Number of ShowCards

--- js/__test__/Search.spec.jsx
+++ js/__test__/Search.spec.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { shallow } from 'enzyme';
-import Search from '../Search';
+/* eslint-disable import/no-extraneous-dependencies */
+import React from "react";
+import { shallow } from "enzyme";
+import  preload from "../../data.json";
+import Search from "../Search";
+import ShowCard from "../ShowCard";
 
-test('Search renders correctly', () => {
+test("Search renders correctly", () => {
   const component = shallow(<Search />);
   expect(component).toMatchSnapshot();
 });
+
+test("Search should render correct amount of shows", () => {
+  const component = shallow(<Search />);
+  expect(component.find(ShowCard).length).toEqual(preload.shows.length);
+});
+
+xtest("Search should renderer correct amount of based on search term", () => {});
